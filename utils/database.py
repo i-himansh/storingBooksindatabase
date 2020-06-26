fbeb277860214310b5ad1890919092f62c860d91
Himanshu Sharma <himanshu.fourtek@gmail.com>
2020-06-26 17:40:54 +0530

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -1,49 +1,42 @@
-import sqlite3
-
+from .database_connection import DatabaseConnection
 
 def create_book_table():
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db')as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
-    connection.commit()
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
-    connection.close()
+
 
 def add_books(name, author):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (name,author))
+    with DatabaseConnection('data.db')as connection:
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (name,author))
 
-    connection.commit()
-    connection.close()
 
 
 def list_all_books():
-   connection = sqlite3.connect('data.db')
-   cursor= connection.cursor()
+    with DatabaseConnection('data.db')as connection:
+        cursor= connection.cursor()
+        cursor.execute('SELECT * FROM books')
+        books = [{"name": row[0],"author": row[1], "read" : row[2]} for row in cursor.fetchall()]
+    return books
 
-   cursor.execute('SELECT * FROM books')
-   books = [{"name": row[0],"author": row[1], "read" : row[2]} for row in cursor.fetchall()]
-
-   connection.close()
-   return  books
 
 
 def read_books(name):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db')as connection:
 
-    cursor.execute("UPDATE books SET read = 1 WHERE name=? ", (name, ))
-    connection.commit()
-    connection.close()
+        cursor = connection.cursor()
+
+        cursor.execute("UPDATE books SET read = 1 WHERE name=? ", (name, ))
+
 
 
 def delete_books(name):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db')as connection:
+        cursor = connection.cursor()
 
-    cursor.execute("DELETE FROM books WHERE name=? ", (name,))
-    connection.commit()
-    connection.close()
+        cursor.execute("DELETE FROM books WHERE name=? ", (name,))
+
 
